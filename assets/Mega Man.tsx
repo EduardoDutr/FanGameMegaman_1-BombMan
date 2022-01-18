b814dc950b8f6384f09abc2e5f9dfbedc88415d7
Eduardo Felipe Pereira Dutra <95875933+Acritz@users.noreply.github.com>
2022-01-18 15:36:52 -0300

    Add files via upload

---
+++ assets/Mega Man.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Mega Man" tilewidth="16" tileheight="16" spacing="1" tilecount="42" columns="6">
+ <image source="../260.png" width="101" height="118"/>
+</tileset>
